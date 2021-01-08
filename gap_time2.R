624cb00f9b8128b3de300d833207d651769521e4
ckkim-rv <68456026+ckkim-rv@users.noreply.github.com>
2021-01-07 20:21:27 -0500

    Update gap_time2.R

--- gap_time2.R
+++ gap_time2.R
@@ -29,11 +29,7 @@ increment <- function(x)
 
 target <- 1992
 t <- 0
-t_1 <- 0
-t_2 <- 0
-t_3 <- 0
-t_4 <- 0
-t_5 <- 0
+
 
 seen <- c()
 
@@ -49,67 +45,5 @@ for (i in 1: nrow(id_year)){
 id_year1 <- id_year[id_year$whaleid %in% seen,]
 
 seen1 <- c()
-
-for (i in 1: nrow(id_year1)){
-  if(id_year1[i,2]==target+1){
-    increment(t_1)
-    seen1 <- c(seen1,id_year1[i,1])
-  }else{
-    1
-  }
-}
-
-id_year2 <- id_year1[id_year1$whaleid %in% seen1,]
-
-seen2 <- c()
-
-for (i in 1: nrow(id_year2)){
-  if(id_year2[i,2]==target+2){
-    increment(t_2)
-    seen2 <- c(seen2,id_year2[i,1])
-  }else{
-    1
-  }
-}
-
-id_year3 <- id_year2[id_year2$whaleid %in% seen2,]
-
-seen3 <- c()
-
-for (i in 1: nrow(id_year3)){
-  if(id_year3[i,2]==target+3){
-    increment(t_3)
-    seen3 <- c(seen3,id_year3[i,1])
-  }else{
-    1
-  }
-}
-
-id_year4 <- id_year3[id_year3$whaleid %in% seen3,]
-
-seen4 <- c()
-
-for (i in 1: nrow(id_year4)){
-  if(id_year4[i,2]==target+4){
-    increment(t_4)
-    seen4 <- c(seen4,id_year4[i,1])
-  }else{
-    1
-  }
-}
-
-id_year5 <- id_year4[id_year4$whaleid %in% seen4,]
-
-seen5 <- c()
-
-for (i in 1: nrow(id_year5)){
-  if(id_year5[i,2]==target+5){
-    increment(t_5)
-    seen5 <- c(seen5,id_year5[i,1])
-  }else{
-    1
-  }
-}
-
 
 
